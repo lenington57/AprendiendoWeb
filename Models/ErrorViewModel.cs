95a21364318604d417e1e6254f66bef0dccf0d8b
Lenington Alfredo Del Orbe Serrano <lenington_delorbe@ucne.edu.do>
2020-06-15 11:23:24 -0400

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AprendiendoWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Persona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AprendiendoWeb.Models
+{
+    public class Persona
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name ="Fecha de Nacimiento")]
+        public DateTime FechaNacimiento { get; set; }
+        public string Nombre { get; set; }
+        [Display(Name = "Teléfono")]
+        public string Telefono { get; set; }
+    }
+}
